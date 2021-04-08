8805412d2407740e403ef256aba575c9680a8141
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-04-08 15:27:12 +0500

    changed notification for time settings

--- src/pages/AdminSettingsPage/index.jsx
+++ src/pages/AdminSettingsPage/index.jsx
@@ -9,12 +9,7 @@ import './styles.css';
 
 const openNotification = (type) => {
   notification[type]({
-    message: 'Notification Title',
-    description:
-      'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-    onClick: () => {
-      console.log('Notification Clicked!');
-    },
+    message: 'Time Changed Successfully',
   });
 };
 
