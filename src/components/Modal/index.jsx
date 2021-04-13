86580a51c120d03cc424e66f34df01d6b4227201
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-04-13 12:01:43 +0500

    separated and integrated ShowModal component

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
+import SearchBox from '../SearchBox';
+
+const ShowModal = ({ currentUser }) => {
+  const [searchField, setSearchField] = useState('');
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const filteredRecord = currentUser.records
+    .filter((user) => user.date.includes(searchField));
+
+  return (
+    <>
+      {' '}
+      <Button type="primary" onClick={showModal}>
+        Record
+      </Button>
+      <Modal title="Record" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <SearchBox
+          placeholder="Search by Date"
+          setSearchField={setSearchField}
+        />
+        <table className="records-table">
+          <thead>
+            <tr>
+              <th className="records-table-cells">Date</th>
+              <th className="records-table-cells">Time In</th>
+              <th className="records-table-cells">Time Out</th>
+              <th className="records-table-cells">Work Hours</th>
+              <th className="records-table-cells">Avg Hours</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredRecord ? (
+              filteredRecord.map((record) => (
+                <tr key={record.id} style={{ border: '1px solid black' }}>
+                  <td className="records-table-cells">{record.date}</td>
+                  <td className="records-table-cells">{record.timeIn}</td>
+                  <td className="records-table-cells">{record.timeOut}</td>
+                  <td className="records-table-cells">{record.workHours}</td>
+                  {currentUser.avgWorkHours !== null ? <td className="records-table-cells">{currentUser.avgWorkHours}</td> : <td className="records-table-cells">{' '}</td>}
+                </tr>
+              ))) : null}
+          </tbody>
+        </table>
+      </Modal>
+    </>
+  );
+};
+
+export default ShowModal;
--- src/pages/UserPunchCardPage/index.jsx
+++ src/pages/UserPunchCardPage/index.jsx
@@ -1,13 +1,11 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable max-len */
+import './styles.css';
 import React, { useEffect, useState } from 'react';
 import { Alert } from 'reactstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { Modal, Button } from 'antd';
+import { Button } from 'antd';
 import { changeAvailabilityUserAction, onNoPunchOutUserAction } from '../../store/actions';
-import './styles.css';
-import SearchBox from '../../components/SearchBox';
 import AvatarLogo from '../../components/Avatar';
+import ShowModal from '../../components/Modal';
 
 const PunchCardPage = () => {
   const users = useSelector((state) => state.users);
@@ -15,33 +13,13 @@ const PunchCardPage = () => {
   const officeStartHours = useSelector((state) => state.officeStartHours);
   const officeEndHours = useSelector((state) => state.officeEndHours);
 
-  const [searchField, setSearchField] = useState('');
   const [late, setLate] = useState(false);
 
-  let filteredRecord;
-  if (currentUser !== '') {
-    filteredRecord = currentUser.records
-      .filter((user) => user.date.includes(searchField));
-  }
   const dispatch = useDispatch();
   let availability;
   if (currentUser) {
     availability = users.find((user) => user.id === currentUser.id).available;
   }
-
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
 
   const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
 
@@ -96,58 +74,19 @@ const PunchCardPage = () => {
                   <>
                     {
                     currentTime < officeEndHours
-                      ? <Button type="button" onClick={() => dispatch(changeAvailabilityUserAction('Not Available'))}>Punch Out</Button>
-                      : (<Button type="button" disabled>Punch Out</Button>)
+                      ? <Button onClick={() => dispatch(changeAvailabilityUserAction('Not Available'))}>Punch Out</Button>
+                      : (<Button disabled>Punch Out</Button>)
                   }
                     {' '}
                     { parseInt((new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })), 10) > parseInt(officeStartHours, 10) + 1
-                      ? (<Button type="button" disabled onClick={() => dispatch(changeAvailabilityUserAction('On Leave'))}>Leave</Button>)
-                      : <Button type="button" onClick={() => dispatch(changeAvailabilityUserAction('On Leave'))}>Leave</Button> }
+                      ? (<Button disabled onClick={() => dispatch(changeAvailabilityUserAction('On Leave'))}>Leave</Button>)
+                      : <Button onClick={() => dispatch(changeAvailabilityUserAction('On Leave'))}>Leave</Button> }
 
                   </>
                 ) : (
-                // <>
-                // { currentTime < officeEndHours?
-                  <Button type="button" onClick={() => dispatch(changeAvailabilityUserAction('Available'))}>Punch In</Button>
-                // : <Button type="button" disabled>Punch In</Button>}
-                // </>
+                  <Button onClick={() => dispatch(changeAvailabilityUserAction('Available'))}>Punch In</Button>
                 )}
-                <>
-                  {' '}
-                  <Button type="primary" onClick={showModal}>
-                    Record
-                  </Button>
-                  <Modal title="Record" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                    <SearchBox
-                      placeholder="Search by Date"
-                      setSearchField={setSearchField}
-                    />
-                    <table className="records-table">
-                      <thead>
-                        <tr>
-                          <th className="records-table-cells">Date</th>
-                          <th className="records-table-cells">Time In</th>
-                          <th className="records-table-cells">Time Out</th>
-                          <th className="records-table-cells">Work Hours</th>
-                          <th className="records-table-cells">Avg Hours</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {filteredRecord ? (
-
-                          filteredRecord.map((record) => (
-                            <tr key={record.id} style={{ border: '1px solid black' }}>
-                              <td className="records-table-cells">{record.date}</td>
-                              <td className="records-table-cells">{record.timeIn}</td>
-                              <td className="records-table-cells">{record.timeOut}</td>
-                              <td className="records-table-cells">{record.workHours}</td>
-                              {currentUser.avgWorkHours !== null ? <td className="records-table-cells">{currentUser.avgWorkHours}</td> : <td className="records-table-cells">{' '}</td>}
-                            </tr>
-                          ))) : null}
-                      </tbody>
-                    </table>
-                  </Modal>
-                </>
+                <ShowModal currentUser={currentUser} />
 
                 { late === true ? (
                   <Alert style={{ textAlign: 'center', color: 'red', marginTop: '15px' }}>
